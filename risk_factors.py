e0274f6cdb71cf6da621bf8376e9fe3990e6aa78
ljsmalbil <louksmalbil@gmail.com>
2020-11-14 18:22:46 +0100

    basic structure added

---
+++ risk_factors.py
@@ -0,0 +1,12 @@
+class RiskFactor()
+    def __init__(self):
+        # Code here
+
+    def foreign_exchange_risk(self):
+        # Code here
+
+    def inflation_riks(self):
+        # Code here
+
+    def default_risk(self):
+        # Code here
